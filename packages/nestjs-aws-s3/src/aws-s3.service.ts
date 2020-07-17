a8ecdb0712f9cd5c86b5d7378a3f635c4598a0ae
Andrew Ghostuhin <me@torinasakura.name>
2020-07-17 22:15:48 +0300

    feat(nestjs-logger): init
    affects: @atlantis-lab/nestjs-logger

--- packages/nestjs-aws-s3/src/aws-s3.service.ts
+++ packages/nestjs-aws-s3/src/aws-s3.service.ts
@@ -1,5 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common'
 import { S3 }                 from 'aws-sdk'
+
+import { Inject, Injectable } from '@nestjs/common'
 
 import { AWS_S3_OPTIONS }     from './aws-s3.constants'
 
---
+++ packages/nestjs-logger/src/logger.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common'
+
+import { INNER_LOGGER }       from './logger.constants'
+
+@Injectable()
+export class Logger {
+  private readonly logger: any
+
+  constructor(
+    @Inject(INNER_LOGGER)
+    logger: any
+  ) {}
+
+  debug(message: any, data?: object): void {
+    this.logger.debug(message, data)
+  }
+
+  trace(message: any, data?: object): void {
+    this.logger.debug(message, data)
+  }
+
+  info(message: any, data?: object): void {
+    this.logger.info(message, data)
+  }
+
+  warn(message: any, data?: object): void {
+    this.logger.warn(message, data)
+  }
+
+  error(message: any, data?: object): void {
+    this.logger.error(message, data)
+  }
+
+  fatal(message: any, data?: object): void {
+    this.logger.error(message, data)
+  }
+}
