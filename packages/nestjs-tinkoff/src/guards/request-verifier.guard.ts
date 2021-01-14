9a4e7f7871875ab8ae2c27cd5a0563ba105fc4c7
Andrew Ghostuhin <me@torinasakura.name>
2021-01-14 14:04:48 +0400

    fix(nestjs-tinkoff): request-verifier guard dont allow a valid requests
    affects: @atlantis-lab/nestjs-tinkoff

--- packages/nestjs-tinkoff/src/guards/request-verifier.guard.ts
+++ packages/nestjs-tinkoff/src/guards/request-verifier.guard.ts
@@ -1,12 +1,14 @@
-import { verifyRequest }                             from '@atlantis-lab/tinkoff-api'
-
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+
+import { TinkoffService }                            from '../services'
 
 @Injectable()
 export class RequestVerifierGuard implements CanActivate {
+  public constructor(private readonly tinkoff: TinkoffService) {}
+
   public canActivate(context: ExecutionContext): boolean {
     const { body } = context.switchToHttp().getRequest()
-    const verified = verifyRequest(body)
+    const verified = this.tinkoff.security.verifyRequest(body)
     return verified
   }
 }
