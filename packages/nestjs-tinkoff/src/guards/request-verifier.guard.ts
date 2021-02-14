8b97fd4ce23815b5d55b5d7f226401130063c2b6
Andrew Ghostuhin <me@torinasakura.name>
2021-02-14 11:14:11 +0800

    Revert "chore(atlantis-lab/nestjs-tinkoff): change redundant verified variable to inline return"
    This reverts commit 765f0f71

--- packages/nestjs-tinkoff/src/guards/request-verifier.guard.ts
+++ packages/nestjs-tinkoff/src/guards/request-verifier.guard.ts
@@ -6,6 +6,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 export class RequestVerifierGuard implements CanActivate {
   public canActivate(context: ExecutionContext): boolean {
     const { body } = context.switchToHttp().getRequest()
-    return verifyRequest(body)
+    const verified = verifyRequest(body)
+    return verified
   }
 }
