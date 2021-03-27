d86b0e90e20ec8b7bb1274f2db967c06291c4ad5
Andrew Ghostuhin <me@torinasakura.name>
2021-03-27 20:03:14 +0400

    chore(nestjs-bus): backward compatibility changes
    affects: @atlantis-lab/nestjs-bus
    
    ISSUES CLOSED: #156

--- packages/nestjs-bus/src/index.ts
+++ packages/nestjs-bus/src/index.ts
@@ -1,4 +1,13 @@
+import { ServiceBus } from '@node-ts/bus-core'
+
+export { ApplicationBootstrap, ServiceBus, HandlerRegistry } from '@node-ts/bus-core'
+export { BUS_SYMBOLS } from './symbols'
 export * from './bus-module'
 export * from './enums'
-export * from './interfaces'
-export * from './providers'
+export { BusModuleOptions }  from './interfaces'
+export * from './decorators'
+
+/**
+ * backward compatibility
+ */
+export const Bus = ServiceBus
