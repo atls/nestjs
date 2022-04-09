5bf81d7f6c9e3bcabe73cc47715826b463498531
Andrew Ghostuhin <me@torinasakura.name>
2022-04-09 19:38:57 +0300

    feat: http proxy header authenticator

--- packages/grpc-playground/src/index.ts
+++ packages/grpc-playground/src/index.ts
@@ -1,4 +1,5 @@
 export * from './module'
 
 export { PrivateKeyAuthenticator } from '@atls/nestjs-grpc-http-proxy'
+export { HeaderAuthenticator } from '@atls/nestjs-grpc-http-proxy'
 export { Authenticator } from '@atls/nestjs-grpc-http-proxy'
