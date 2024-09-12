96783bf70a3c28709f4ecf3a0ca2f01b31661cc2
Andrew Ghostuhin <me@torinasakura.name>
2024-09-13 00:24:28 +0300

    Revert "feat(validation): init package"
    This reverts commit c7509e4f24c2f96fe57ba7308d2c2277e9f67502.

--- packages/nestjs-validation/src/errors/validation.error.ts
+++
@@ -1,9 +0,0 @@
-import type { ValidationError as VError } from 'class-validator'
-
-export class ValidationError extends Error {
-  constructor(public readonly errors: Array<VError>) {
-    super()
-
-    this.message = 'Validation failed'
-  }
-}
