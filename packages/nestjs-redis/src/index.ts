7d520e9f3c458bc63e143d819813ad0af3c45d8b
Andrew Ghostuhin <me@torinasakura.name>
2024-09-18 01:05:54 +0300

    feat(nestjs-redis): init package

---
+++ packages/nestjs-redis/src/index.ts
@@ -0,0 +1,2 @@
+export * from 'ioredis'
+export * from './module/index.js'
---
+++ packages/nestjs-redis/src/module/index.ts
@@ -0,0 +1,4 @@
+export * from './redis.module.constants.js'
+export * from './redis.config-factory.js'
+export * from './redis.factory.js'
+export * from './redis.module.js'
