efea6d0a688d5006696910edb6140fbcc31c3911
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-04 17:14:08 +0300

    fix: add index.ts for module

---
+++ packages/nestjs-batch-queue/src/module/index.ts
@@ -0,0 +1,4 @@
+export * from './batch-queue-module-options.interface.js'
+export * from './batch-queue.constants.js'
+export * from './batch-queue.module.js'
+export * from './batch-queue.providers.js'
