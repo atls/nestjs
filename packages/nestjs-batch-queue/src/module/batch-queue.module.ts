546ccc13433d83efef113617d878be5e291c67a4
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-08 16:36:33 +0300

    refactor: move providers creation to separate file

--- packages/nestjs-batch-queue/src/module/batch-queue.module.ts
+++ packages/nestjs-batch-queue/src/module/batch-queue.module.ts
@@ -1,159 +1,64 @@
-import type { DynamicModule }                from '@nestjs/common'
-import type { Provider }                     from '@nestjs/common'
+import type { DynamicModule }                      from '@nestjs/common'
+import type { Provider }                           from '@nestjs/common'
 
-import type { BatchQueueModuleAsyncOptions } from './batch-queue-module-options.interface.js'
-import type { BatchQueueModuleOptions }      from './batch-queue-module-options.interface.js'
-import type { BatchQueueOptionsFactory }     from './batch-queue-module-options.interface.js'
+import type { BatchQueueModuleAsyncOptions }       from './batch-queue-module-options.interface.js'
+import type { BatchQueueModuleOptions }            from './batch-queue-module-options.interface.js'
+import type { BatchQueueOptionsFactory }           from './batch-queue-module-options.interface.js'
 
-import { Module }                            from '@nestjs/common'
+import { Module }                                  from '@nestjs/common'
 
-import { BatchQueue }                        from '../batch-queue/index.js'
-import { Consumer }                          from '../batch-queue/index.js'
-import { Producer }                          from '../batch-queue/index.js'
-import { Checker }                           from '../batch-queue/index.js'
-import { StateHandler }                      from '../batch-queue/index.js'
-import { BATCH_QUEUE }                       from './constants/index.js'
-import { BATCH_QUEUE_MODULE_OPTIONS }        from './constants/index.js'
-import { BATCH_QUEUE_CONSUMER }              from './constants/index.js'
-import { BATCH_QUEUE_PRODUCER }              from './constants/index.js'
-import { BATCH_QUEUE_CHECKER }               from './constants/index.js'
-import { BATCH_QUEUE_STATE_HANDLER }         from './constants/index.js'
-import { MEMORY_CHECKER_OPTIONS }            from './constants/index.js'
+import { BATCH_QUEUE_MODULE_OPTIONS }              from './constants/index.js'
+import { createBatchQueueSyncProvider }            from './batch-queue.providers.js'
+import { createBatchQueueAsyncProvider }           from './batch-queue.providers.js'
+import { createMemoryCheckerOptionsSyncProvider }  from './batch-queue.providers.js'
+import { createMemoryCheckerOptionsAsyncProvider } from './batch-queue.providers.js'
+import { createBatchQueueConsumerProvider }        from './batch-queue.providers.js'
+import { createBatchQueueProducerProvider }        from './batch-queue.providers.js'
+import { createBatchQueueCheckerProvider }         from './batch-queue.providers.js'
+import { createBatchQueueStateHandlerProvider }    from './batch-queue.providers.js'
+import { exportsProviders }                        from './batch-queue.providers.js'
 
 @Module({})
 export class BatchQueueModule {
-  static register(options: BatchQueueModuleOptions): DynamicModule {
-    const batchQueueProvider = {
-      provide: BATCH_QUEUE,
-      useValue: new BatchQueue(options.core),
-    }
-
-    const memoryCheckerOptionsProvider = {
-      provide: MEMORY_CHECKER_OPTIONS,
-      useValue: options.memoryCheckerOptions,
-    }
-
-    const consumerProvider = {
-      provide: BATCH_QUEUE_CONSUMER,
-      useFactory: (batchQueue: BatchQueue<any>): Consumer => new Consumer(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const producerProvider = {
-      provide: BATCH_QUEUE_PRODUCER,
-      useFactory: (batchQueue: BatchQueue<any>): Producer<any> => new Producer(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const checkerProvider = {
-      provide: BATCH_QUEUE_CHECKER,
-      useFactory: (batchQueue: BatchQueue<any>): Checker => new Checker(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const stateHandlerProvider = {
-      provide: BATCH_QUEUE_STATE_HANDLER,
-      useFactory: (batchQueue: BatchQueue<any>): StateHandler => new StateHandler(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    return {
-      module: BatchQueueModule,
-      providers: [
-        batchQueueProvider,
-        consumerProvider,
-        producerProvider,
-        checkerProvider,
-        stateHandlerProvider,
-        memoryCheckerOptionsProvider,
-      ],
-      exports: [
-        BATCH_QUEUE_CONSUMER,
-        BATCH_QUEUE_PRODUCER,
-        BATCH_QUEUE_CHECKER,
-        BATCH_QUEUE_STATE_HANDLER,
-        MEMORY_CHECKER_OPTIONS,
-      ],
-    }
-  }
+  static register = (options: BatchQueueModuleOptions): DynamicModule => ({
+    module: BatchQueueModule,
+    providers: [
+      createBatchQueueSyncProvider(options),
+      createMemoryCheckerOptionsSyncProvider(options),
+      createBatchQueueConsumerProvider(),
+      createBatchQueueProducerProvider(),
+      createBatchQueueCheckerProvider(),
+      createBatchQueueStateHandlerProvider(),
+    ],
+    exports: exportsProviders,
+  })
 
   static registerAsync(options: BatchQueueModuleAsyncOptions): DynamicModule {
     return {
       module: BatchQueueModule,
       imports: options.imports || [],
       providers: [...this.createAsyncProviders(options)],
-      exports: [
-        BATCH_QUEUE_CONSUMER,
-        BATCH_QUEUE_PRODUCER,
-        BATCH_QUEUE_CHECKER,
-        BATCH_QUEUE_STATE_HANDLER,
-        MEMORY_CHECKER_OPTIONS,
-      ],
+      exports: exportsProviders,
     }
   }
 
   private static createAsyncProviders(options: BatchQueueModuleAsyncOptions): Array<Provider> {
-    const batchQueueProvider = {
-      provide: BATCH_QUEUE,
-      useFactory: (opt: BatchQueueModuleOptions): BatchQueue<any> => new BatchQueue(opt.core),
-      inject: [BATCH_QUEUE_MODULE_OPTIONS],
-    }
-
-    const memoryCheckerOptionsProvider = {
-      provide: MEMORY_CHECKER_OPTIONS,
-      useFactory: (opt: BatchQueueModuleOptions) => opt.memoryCheckerOptions,
-      inject: [BATCH_QUEUE_MODULE_OPTIONS],
-    }
-
-    const consumerProvider = {
-      provide: BATCH_QUEUE_CONSUMER,
-      useFactory: (batchQueue: BatchQueue<any>): Consumer => new Consumer(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const producerProvider = {
-      provide: BATCH_QUEUE_PRODUCER,
-      useFactory: (batchQueue: BatchQueue<any>): Producer<any> => new Producer(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const checkerProvider = {
-      provide: BATCH_QUEUE_CHECKER,
-      useFactory: (batchQueue: BatchQueue<any>): Checker => new Checker(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    const stateHandlerProvider = {
-      provide: BATCH_QUEUE_STATE_HANDLER,
-      useFactory: (batchQueue: BatchQueue<any>): StateHandler => new StateHandler(batchQueue),
-      inject: [BATCH_QUEUE],
-    }
-
-    if (options.useExisting || options.useFactory) {
-      return [
-        this.createAsyncOptionsProvider(options),
-        batchQueueProvider,
-        consumerProvider,
-        producerProvider,
-        checkerProvider,
-        stateHandlerProvider,
-        memoryCheckerOptionsProvider,
-      ]
-    }
-
-    return [
+    const providers: Array<Provider> = [
       this.createAsyncOptionsProvider(options),
-      batchQueueProvider,
-      consumerProvider,
-      producerProvider,
-      checkerProvider,
-      stateHandlerProvider,
-      memoryCheckerOptionsProvider,
-      {
+      createBatchQueueAsyncProvider(),
+      createMemoryCheckerOptionsAsyncProvider(),
+      createBatchQueueConsumerProvider(),
+      createBatchQueueProducerProvider(),
+      createBatchQueueCheckerProvider(),
+      createBatchQueueStateHandlerProvider(),
+    ]
+    if (!(options.useExisting || options.useFactory)) {
+      providers.push({
         provide: options.useClass!,
         useClass: options.useClass!,
-      },
-    ]
+      })
+    }
+    return providers
   }
 
   private static createAsyncOptionsProvider(options: BatchQueueModuleAsyncOptions): Provider {
--- packages/nestjs-batch-queue/src/module/batch-queue.providers.ts
+++ packages/nestjs-batch-queue/src/module/batch-queue.providers.ts
@@ -2,15 +2,75 @@ import type { Provider }                from '@nestjs/common'
 
 import type { BatchQueueModuleOptions } from './batch-queue-module-options.interface.js'
 
+import { BatchQueue }                   from '../batch-queue/index.js'
+import { Consumer }                     from '../batch-queue/index.js'
+import { Producer }                     from '../batch-queue/index.js'
+import { Checker }                      from '../batch-queue/index.js'
+import { StateHandler }                 from '../batch-queue/index.js'
 import { BATCH_QUEUE_MODULE_OPTIONS }   from './constants/index.js'
+import { BATCH_QUEUE }                  from './constants/index.js'
+import { MEMORY_CHECKER_OPTIONS }       from './constants/index.js'
+import { BATCH_QUEUE_CONSUMER }         from './constants/index.js'
+import { BATCH_QUEUE_PRODUCER }         from './constants/index.js'
+import { BATCH_QUEUE_CHECKER }          from './constants/index.js'
+import { BATCH_QUEUE_STATE_HANDLER }    from './constants/index.js'
 
-export const createBatchQueueOptionsProvider = (
-  options: BatchQueueModuleOptions
-): Array<Provider> => [
-  {
-    provide: BATCH_QUEUE_MODULE_OPTIONS,
-    useValue: options,
-  },
+export const createBatchQueueSyncProvider = (
+  batchQueueModuleOptions: BatchQueueModuleOptions
+): Provider => ({
+  provide: BATCH_QUEUE,
+  useValue: new BatchQueue(batchQueueModuleOptions.core),
+})
+
+export const createBatchQueueAsyncProvider = (): Provider => ({
+  provide: BATCH_QUEUE,
+  useFactory: (batchQueueModuleOptions: BatchQueueModuleOptions): BatchQueue<any> =>
+    new BatchQueue(batchQueueModuleOptions.core),
+  inject: [BATCH_QUEUE_MODULE_OPTIONS],
+})
+
+export const createMemoryCheckerOptionsSyncProvider = (
+  batchQueueModuleOptions: BatchQueueModuleOptions
+): Provider => ({
+  provide: MEMORY_CHECKER_OPTIONS,
+  useValue: batchQueueModuleOptions.memoryCheckerOptions,
+})
+
+export const createMemoryCheckerOptionsAsyncProvider = (): Provider => ({
+  provide: MEMORY_CHECKER_OPTIONS,
+  useFactory: (batchQueueModuleOptions: BatchQueueModuleOptions): any =>
+    batchQueueModuleOptions.memoryCheckerOptions,
+  inject: [BATCH_QUEUE_MODULE_OPTIONS],
+})
+
+export const createBatchQueueConsumerProvider = (): Provider => ({
+  provide: BATCH_QUEUE_CONSUMER,
+  useFactory: (batchQueue: BatchQueue<any>): Consumer => new Consumer(batchQueue),
+  inject: [BATCH_QUEUE],
+})
+
+export const createBatchQueueProducerProvider = (): Provider => ({
+  provide: BATCH_QUEUE_PRODUCER,
+  useFactory: (batchQueue: BatchQueue<any>): Producer<any> => new Producer(batchQueue),
+  inject: [BATCH_QUEUE],
+})
+
+export const createBatchQueueCheckerProvider = (): Provider => ({
+  provide: BATCH_QUEUE_CHECKER,
+  useFactory: (batchQueue: BatchQueue<any>): Checker => new Checker(batchQueue),
+  inject: [BATCH_QUEUE],
+})
+
+export const createBatchQueueStateHandlerProvider = (): Provider => ({
+  provide: BATCH_QUEUE_STATE_HANDLER,
+  useFactory: (batchQueue: BatchQueue<any>): StateHandler => new StateHandler(batchQueue),
+  inject: [BATCH_QUEUE],
+})
+
+export const exportsProviders = [
+  BATCH_QUEUE_CONSUMER,
+  BATCH_QUEUE_PRODUCER,
+  BATCH_QUEUE_CHECKER,
+  BATCH_QUEUE_STATE_HANDLER,
+  MEMORY_CHECKER_OPTIONS,
 ]
-
-export const createBatchQueueExportsProvider = (): Array<Provider> => []
