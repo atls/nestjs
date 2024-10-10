43be19b63dde6b9a937a435f30a79054f096a18e
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 23:37:21 +0300

    docs: add JSDoc to Checker

--- packages/nestjs-batch-queue/src/proxy-clases/checker.ts
+++ packages/nestjs-batch-queue/src/proxy-clases/checker.ts
@@ -4,10 +4,16 @@ import type { CheckName }    from '../check-manager/index.js'
 export class Checker {
   constructor(private checkManager: CheckManager) {}
 
+  /**
+   * Delegates to {@link CheckManager.createCheck}
+   */
   public createCheck(checkName: CheckName, initialState: boolean): void {
     this.checkManager.createCheck(checkName, initialState)
   }
 
+  /**
+   * Delegates to {@link CheckManager.changeState}
+   */
   public async changeState(checkName: CheckName, state: boolean): Promise<void> {
     await this.checkManager.changeState(checkName, state)
   }
