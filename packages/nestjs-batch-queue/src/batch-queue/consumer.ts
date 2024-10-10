790cde6f31622343745faec251f6b1387383b55d
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 13:33:18 +0300

    docs: add JSDoc to Consumer

--- packages/nestjs-batch-queue/src/batch-queue/consumer.ts
+++ packages/nestjs-batch-queue/src/batch-queue/consumer.ts
@@ -4,6 +4,9 @@ import type { ProcessorFn } from './types/index.js'
 export class Consumer {
   constructor(private batchQueue: BatchQueue<any>) {}
 
+  /**
+   * Delegates to {@link BatchQueue.processBatch}
+   */
   public consume<T>(processor: ProcessorFn<T>): void {
     this.batchQueue.processBatch(processor)
   }
