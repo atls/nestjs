7ab7f946af99c61e77f96685b94be0dea191d042
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 13:32:36 +0300

    docs: add JSDoc to Checker

--- packages/nestjs-batch-queue/src/batch-queue/checker.ts
+++ packages/nestjs-batch-queue/src/batch-queue/checker.ts
@@ -6,11 +6,17 @@ import type { CheckOnAdd } from './types/index.js'
 export class Checker {
   constructor(private batchQueue: BatchQueue<any>) {}
 
+  /**
+   * Delegates to {@link BatchQueue.createCheck}
+   */
   public createCheck(checkName: CheckName, initialState: boolean): Checks {
     const checks = this.batchQueue.createCheck(checkName, initialState)
     return checks
   }
 
+  /**
+   * Delegates to {@link BatchQueue.createCheckOnAdd}
+   */
   public createCheckOnAdd(
     checkName: CheckName,
     checkOnAdd: CheckOnAdd,
