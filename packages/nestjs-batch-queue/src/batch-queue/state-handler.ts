c7d4614addcccb027a1542e89b838fcb44ab768b
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 13:34:53 +0300

    docs: add JSDoc to StateHandler

--- packages/nestjs-batch-queue/src/batch-queue/state-handler.ts
+++ packages/nestjs-batch-queue/src/batch-queue/state-handler.ts
@@ -5,10 +5,16 @@ import type { OnChangeStateToFailCallback } from './types/index.js'
 export class StateHandler {
   constructor(private batchQueue: BatchQueue<any>) {}
 
+  /**
+   * Delegates to {@link BatchQueue.onChangeTotalStateToOk}
+   */
   public onChangeTotalStateToOk(callback: OnChangeStateToOkCallback): void {
     this.batchQueue.onChangeTotalStateToOk(callback)
   }
 
+  /**
+   * Delegates to {@link BatchQueue.onChangeTotalStateToFail}
+   */
   public onChangeTotalStateToFail(callback: OnChangeStateToFailCallback): void {
     this.batchQueue.onChangeTotalStateToFail(callback)
   }
