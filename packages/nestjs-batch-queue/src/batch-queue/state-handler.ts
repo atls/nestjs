0fec37415cf64f63aee8d34ef69dc6eb080a9163
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 21:03:30 +0300

    refactor: state handler depend on check manager

--- packages/nestjs-batch-queue/src/batch-queue/state-handler.ts
+++ packages/nestjs-batch-queue/src/batch-queue/state-handler.ts
@@ -1,21 +1,11 @@
-import type { BatchQueue }                  from './batch-queue.js'
-import type { OnChangeStateToOkCallback }   from './types/index.js'
-import type { OnChangeStateToFailCallback } from './types/index.js'
+import type { CheckManager }        from './check-manager.js'
+import type { CheckName }           from './types/index.js'
+import type { ChangeStateCallback } from './types/index.js'
 
 export class StateHandler {
-  constructor(private batchQueue: BatchQueue<any>) {}
+  constructor(private checkManager: CheckManager) {}
 
-  /**
-   * Delegates to {@link BatchQueue.onChangeTotalStateToOk}
-   */
-  public onChangeTotalStateToOk(callback: OnChangeStateToOkCallback): void {
-    this.batchQueue.onChangeTotalStateToOk(callback)
-  }
-
-  /**
-   * Delegates to {@link BatchQueue.onChangeTotalStateToFail}
-   */
-  public onChangeTotalStateToFail(callback: OnChangeStateToFailCallback): void {
-    this.batchQueue.onChangeTotalStateToFail(callback)
+  public handleChangeState(checkName: CheckName, callback: ChangeStateCallback): void {
+    this.checkManager.handleChangeState(checkName, callback)
   }
 }
