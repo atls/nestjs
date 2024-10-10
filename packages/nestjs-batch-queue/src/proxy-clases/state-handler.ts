214eab0efe5835f1e4c6f131dee2c4c65ec1ac98
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 23:40:41 +0300

    docs: add JSDoc to StateHandler

--- packages/nestjs-batch-queue/src/proxy-clases/state-handler.ts
+++ packages/nestjs-batch-queue/src/proxy-clases/state-handler.ts
@@ -5,6 +5,9 @@ import type { ChangeStateCallback } from '../check-manager/index.js'
 export class StateHandler {
   constructor(private checkManager: CheckManager) {}
 
+  /**
+   * Delegates to {@link CheckManager.handleChangeState}
+   */
   public handleChangeState(checkName: CheckName, callback: ChangeStateCallback): void {
     this.checkManager.handleChangeState(checkName, callback)
   }
