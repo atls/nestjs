9c9154392ab6664cedec8ba2fe7ff76cd02a9707
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 20:57:31 +0300

    refactor: use CheckManager in BatchQueue

--- packages/nestjs-batch-queue/src/batch-queue/batch-queue.ts
+++ packages/nestjs-batch-queue/src/batch-queue/batch-queue.ts
@@ -1,16 +1,9 @@
 import type { BatchQueueOptions }              from './batch-queue.interface.js'
+import type { CheckManager }                   from './check-manager.js'
 import type { QueueName }                      from './types/index.js'
-import type { CheckName }                      from './types/index.js'
-import type { OnChangeStateToOkCallback }      from './types/index.js'
-import type { OnChangeStateToFailCallback }    from './types/index.js'
 import type { ProcessorFn }                    from './types/index.js'
 import type { AddCond }                        from './types/index.js'
 import type { AddManyCond }                    from './types/index.js'
-import type { Checks }                         from './types/index.js'
-import type { CheckOk }                        from './types/index.js'
-import type { CheckFail }                      from './types/index.js'
-import type { CheckOnAdd }                     from './types/index.js'
-import type { OnAddConfig }                    from './types/index.js'
 
 import { MaxQueueCountError }                  from '../errors/index.js'
 import { MaxQueueLengthExceededError }         from '../errors/index.js'
@@ -21,20 +14,6 @@ import { Mutex }                               from './mutex.js'
 export class BatchQueue<T> {
   private queues: Map<QueueName, Array<T>> = new Map()
 
-  private checkStates: Map<
-    CheckName,
-    {
-      state: boolean
-      onAddConfig?: OnAddConfig
-    }
-  > = new Map()
-
-  private onOkCallbacks: Array<OnChangeStateToOkCallback> = []
-
-  private onFailCallbacks: Array<OnChangeStateToFailCallback> = []
-
-  private options: BatchQueueOptions
-
   private totalQueueLength: number = 0
 
   private timers: Map<QueueName, NodeJS.Timeout> = new Map()
@@ -43,9 +22,10 @@ export class BatchQueue<T> {
 
   private mutexes: Map<QueueName, Mutex> = new Map()
 
-  constructor(options: BatchQueueOptions) {
-    this.options = options
-  }
+  constructor(
+    private readonly options: BatchQueueOptions,
+    private readonly checkManager: CheckManager
+  ) {}
 
   /**
    * Add a single item to the specified queue.
@@ -77,7 +57,8 @@ export class BatchQueue<T> {
   public async addMany(addManyCond: AddManyCond<T>): Promise<void> {
     const unlock = await this.getMutex(addManyCond.queueName).lock()
     try {
-      this.checkAllChecks()
+      const goodState = this.checkManager.getState()
+      if (!goodState) throw new CheckFailedError()
 
       const { queueName, items } = addManyCond
 
@@ -102,8 +83,6 @@ export class BatchQueue<T> {
         throw new MaxTotalLengthOfQueuesExceededError()
       }
 
-      await this.checkOnAddChecks(items.length)
-
       queue.push(...items)
       this.totalQueueLength += items.length
 
@@ -120,129 +99,6 @@ export class BatchQueue<T> {
    */
   public processBatch(processorFn: ProcessorFn<T>): void {
     this.processorFn = processorFn
-  }
-
-  /**
-   * Creates a new check for the batch queue.
-   * The check has an initial state and can be triggered to pass or fail.
-   *
-   * @param {CheckName} checkName - The name of the check.
-   * @param {boolean} initialState - The initial state of the check (true for OK, false for fail).
-   * @returns {Checks} - The object containing methods to pass (`checkOk`) or fail (`checkFail`) the check.
-   */
-  public createCheck(checkName: CheckName, initialState: boolean): Checks {
-    if (!this.checkStates.has(checkName)) {
-      this.checkStates.set(checkName, { state: initialState })
-    }
-
-    const checkOk: CheckOk = async () => {
-      const beforeTotalCheck = this.totalCheck()
-      const checkState = this.checkStates.get(checkName)
-      if (checkState) {
-        checkState.state = true
-      }
-      if (beforeTotalCheck) return
-      const afterTotalCheck = this.totalCheck()
-      if (afterTotalCheck) {
-        await this.triggerOkCallbacks()
-      }
-    }
-
-    const checkFail: CheckFail = async () => {
-      const beforeTotalCheck = this.totalCheck()
-      const checkState = this.checkStates.get(checkName)
-      if (checkState) {
-        checkState.state = false
-      }
-      if (!beforeTotalCheck) return
-      const afterTotalCheck = this.totalCheck()
-      if (!afterTotalCheck) {
-        await this.triggerFailCallbacks()
-      }
-    }
-
-    return { checkOk, checkFail }
-  }
-
-  /**
-   * Creates a check that is evaluated on adding items to the queue.
-   * The check can be configured to run after a specified number of items are added.
-   *
-   * @param {CheckName} checkName - The name of the check.
-   * @param {CheckOnAdd} checkOnAdd - The function to evaluate the check on item addition.
-   * @param {number} checkEveryItem - The interval at which the check should be triggered (number of items).
-   */
-  public createCheckOnAdd(
-    checkName: CheckName,
-    checkOnAdd: CheckOnAdd,
-    checkEveryItem: number
-  ): void {
-    if (!this.checkStates.has(checkName)) {
-      this.checkStates.set(checkName, { state: true })
-    }
-    const checkState = this.checkStates.get(checkName)!
-    checkState.onAddConfig = { checkOnAdd, checkEveryItem, currentItemCounter: 0 }
-  }
-
-  /**
-   * Registers a callback to be invoked when total state change to OK.
-   *
-   * @param {OnChangeStateToOkCallback} callback - The callback function to be invoked when the total state changes to OK.
-   */
-  public onChangeTotalStateToOk(callback: OnChangeStateToOkCallback): void {
-    this.onOkCallbacks.push(callback)
-  }
-
-  /**
-   * Registers a callback to be invoked when total state change to failed.
-   *
-   * @param {OnChangeStateToFailCallback} callback - The callback function to be invoked when the total state changes to failed.
-   */
-  public onChangeTotalStateToFail(callback: OnChangeStateToFailCallback): void {
-    this.onFailCallbacks.push(callback)
-  }
-
-  private totalCheck(): boolean {
-    for (const { state } of this.checkStates.values()) {
-      if (!state) {
-        return false
-      }
-    }
-    return true
-  }
-
-  private checkAllChecks(): void {
-    const failedChecks = []
-    for (const [checkName, { state }] of this.checkStates.entries()) {
-      if (!state) {
-        failedChecks.push(checkName)
-      }
-    }
-    if (failedChecks.length > 0) {
-      throw new CheckFailedError(failedChecks)
-    }
-  }
-
-  private async checkOnAddChecks(itemsLength: number): Promise<void> {
-    for (const [checkName, { onAddConfig }] of this.checkStates.entries()) {
-      // eslint-disable-next-line no-continue
-      if (!onAddConfig) continue
-
-      const { checkOnAdd, checkEveryItem, currentItemCounter } = onAddConfig
-      let itemCounter = currentItemCounter + itemsLength
-
-      while (itemCounter >= checkEveryItem) {
-        // eslint-disable-next-line no-await-in-loop
-        const okCheck = await checkOnAdd()
-
-        if (!okCheck) {
-          throw new CheckFailedError([checkName])
-        }
-
-        itemCounter -= checkEveryItem
-      }
-      onAddConfig.currentItemCounter = itemCounter
-    }
   }
 
   private startTimerIfNecessary(queueName: QueueName): void {
@@ -270,16 +126,6 @@ export class BatchQueue<T> {
     }
 
     this.mutexes.delete(queueName)
-  }
-
-  private async triggerOkCallbacks(): Promise<void> {
-    const promises = Promise.all(this.onOkCallbacks.map(async (callback) => callback()))
-    await promises
-  }
-
-  private async triggerFailCallbacks(): Promise<void> {
-    const promises = Promise.all(this.onFailCallbacks.map(async (callback) => callback()))
-    await promises
   }
 
   private getMutex(queueName: QueueName): Mutex {
