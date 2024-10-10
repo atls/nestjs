537ec289735174148b02bb4cdd8123904ad6dc74
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-10 13:34:17 +0300

    docs: add JSDoc to Producer

--- packages/nestjs-batch-queue/src/batch-queue/producer.ts
+++ packages/nestjs-batch-queue/src/batch-queue/producer.ts
@@ -4,6 +4,9 @@ import type { QueueName }  from './types/index.js'
 export class Producer<T> {
   constructor(private batchQueue: BatchQueue<T>) {}
 
+  /**
+   * Delegates to {@link BatchQueue.add}
+   */
   async produce(queueName: QueueName, data: T): Promise<void> {
     await this.batchQueue.add({ queueName, item: data })
   }
