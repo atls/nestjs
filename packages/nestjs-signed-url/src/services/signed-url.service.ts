9757f0fd65f9dd80156a513de0b61b6361aa6d0b
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-10-15 13:18:21 +0300

    style: signed url service use abstract storage type insead any

--- packages/nestjs-signed-url/src/services/signed-url.service.ts
+++ packages/nestjs-signed-url/src/services/signed-url.service.ts
@@ -1,27 +1,27 @@
-import type { SignUrlOptions } from '../storage/index.js'
-import type { SignedUrl }      from '../storage/index.js'
+import type { SignUrlOptions }  from '../storage/index.js'
+import type { SignedUrl }       from '../storage/index.js'
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import type { AbstractStorage } from '../storage/index.js'
 
-import { Inject }              from '@nestjs/common'
-import { Injectable }          from '@nestjs/common'
+import { Inject }               from '@nestjs/common'
+import { Injectable }           from '@nestjs/common'
 
-import { STORAGE }             from '../storage/index.js'
+import { STORAGE }              from '../storage/index.js'
 
 @Injectable()
 export class SignedUrlService {
   @Inject(STORAGE)
-  storage: any
+  storage: AbstractStorage
 
   async generateWriteUrl(
     bucket: string,
     filename: string,
     options: SignUrlOptions
   ): Promise<SignedUrl> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    return this.storage.generateWriteUrl(bucket, filename, options) as Promise<SignedUrl>
+    return this.storage.generateWriteUrl(bucket, filename, options)
   }
 
   async generateReadUrl(bucket: string, filename: string): Promise<SignedUrl> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    return this.storage.generateReadUrl(bucket, filename) as Promise<SignedUrl>
+    return this.storage.generateReadUrl(bucket, filename)
   }
 }
