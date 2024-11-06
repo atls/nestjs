c9377e6f16441981e059c13ab2bba910ed2326a3
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-11-06 15:19:51 +0300

    chore: jsdoc for proto types

--- packages/nestjs-proto-types/src/index.ts
+++ packages/nestjs-proto-types/src/index.ts
@@ -1,1 +1,14 @@
+/**
+ * Declares a module for `.proto` files, which are commonly used for defining
+ * Protocol Buffers (protobuf) schemas.
+ *
+ * This declaration allows TypeScript to recognize `.proto` files as modules
+ * that can be imported directly in TypeScript projects, enabling integration
+ * with Protocol Buffers in environments where `.proto` files are parsed or
+ * used as source definitions for generating classes and types.
+ *
+ * @module
+ * @typedef {string} ProtoFile - Represents the imported `.proto` file as a string
+ */
+// @ts-expect-error
 declare module '*.proto'
