7b335b15c966cf797d7ab22a77b69bc0b6b0b9b8
Dmitry <dimafomin02@gmail.com>
2025-01-24 14:35:16 +0300

    fix(nestjs-grpc-reflection): lint and type errors

--- packages/nestjs-grpc-reflection/src/grpc/grpc.reflector.ts
+++ packages/nestjs-grpc-reflection/src/grpc/grpc.reflector.ts
@@ -28,28 +28,30 @@ export class GrpcReflector implements OnModuleInit {
       : [this.options.protoPath]
 
     for (const protoPath of protoPaths) {
-      // @ts-expect-error
-      const packageDefinition = loadSync(protoPath, this.options.loader)
-      const grpcContext = loadPackageDefinition(packageDefinition)
+      if (protoPath) {
+        // eslint-disable-next-line n/no-sync
+        const packageDefinition = loadSync(protoPath, this.options.loader)
+        const grpcContext = loadPackageDefinition(packageDefinition)
 
-      const packageNames = Array.isArray(this.options.package)
-        ? this.options.package
-        : [this.options.package]
+        const packageNames = Array.isArray(this.options.package)
+          ? this.options.package
+          : [this.options.package]
 
-      for (const packageName of packageNames) {
-        const grpcPkg = this.lookupPackage(grpcContext, packageName)
+        for (const packageName of packageNames) {
+          const grpcPkg = this.lookupPackage(grpcContext, packageName)
 
-        if (grpcPkg) {
-          for (const definition of grpcServer.getServiceNames(grpcPkg)) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            this.registry.addService(definition.service.service)
+          if (grpcPkg) {
+            for (const definition of grpcServer.getServiceNames(grpcPkg)) {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+              this.registry.addService(definition.service.service)
+            }
           }
         }
       }
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
   public lookupPackage(root: any, packageName: string): any {
     let pkg = root
 
