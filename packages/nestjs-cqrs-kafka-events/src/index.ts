251222dc57c33baae0cc0986d878208ebe79827e
Andrew Ghostuhin <me@torinasakura.name>
2025-03-14 04:04:04 +0300

    feat(nestjs-cqrs-kafka-events): init

---
+++ packages/nestjs-cqrs-kafka-events/src/index.ts
@@ -0,0 +1,4 @@
+export * from '@atls/nestjs-kafka'
+
+export * from './messaging/index.js'
+export * from './module/index.js'
---
+++ packages/nestjs-cqrs-kafka-events/src/module/index.ts
@@ -0,0 +1,3 @@
+export * from './cqrs-kafka-events.module.interfaces.js'
+export * from './cqrs-kafka-events.module.constants.js'
+export * from './cqrs-kafka-events.module.js'
