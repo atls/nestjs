1106bc99f366c7e60132cbe067596b735f62ce94
Dmitry Fomin <80476878+Amiditin@users.noreply.github.com>
2026-01-09 16:27:33 +0300

    chore(packages): trigger release (#375)

--- packages/nestjs-connectrpc/src/index.ts
+++ packages/nestjs-connectrpc/src/index.ts
@@ -1,4 +1,4 @@
-export * from './connectrpc.decorators.js'
 export * from './connectrpc.interfaces.js'
+export * from './connectrpc.decorators.js'
 export * from './connectrpc.strategy.js'
 export * from './connectrpc.server.js'
